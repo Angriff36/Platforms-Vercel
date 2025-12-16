cf570dbbf13c5bedde283e19446b9f74cf86b4a3
Angriff36 <unashamed366@gmail.com>
2025-12-16 10:45:44 -0800

    chore(monorepo): scaffold monorepo skeleton and CI-based Vercel deployments

---
+++ apps/web/.next/static/chunks/apps_web_app_layout_tsx_626121c1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__dc4e1ff2._.css",
+  "static/chunks/node_modules__pnpm_009d7d74._.js"
+],
+    source: "dynamic"
+});
---
+++ apps/web/components/sales/PipelineView.tsx
@@ -0,0 +1,3 @@
+export default function PipelineView(){
+  return null;
+}
